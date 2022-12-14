3a8f6aa01af7d519c2fe0cd33dd18cd231388b35
ericpkatz <ericpkatz@gmail.com>
2022-12-13 19:29:21 -0500

    commit

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const Sequelize = require('sequelize');
 const { UUID, UUIDV4, STRING } = Sequelize;
 const conn = new Sequelize(process.env.DATABASE_URL || 'postgres://localhost/leaderboard_db');
 const redis = require('redis');
+console.log(process.env.REDIS_URL);
 const client = redis.createClient(process.env.REDIS_URL);
 
 app.get('/leaderboard/', async(req, res, next)=> {
